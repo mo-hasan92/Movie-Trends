cdbb052bd8d32c92214cc15565754e5eafd5ed82
mo-hasan92 <102214961+mo-hasan92@users.noreply.github.com>
2025-08-31 16:10:34 +0200

    `Removed entire Android project directory`

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'modern-app',
-  webDir: 'www'
-};
-
-export default config;
